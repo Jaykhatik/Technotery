270c55b2c9ace284a676082d7a38af1208d6af6a
Jaykhatik <jaykhatik1905@gmail.com>
2026-06-12 13:18:35 +0530

    add tanstack folder

---
+++ TenStack(React Query)/tanstack-learning/src/app/layout.tsx
@@ -0,0 +1,19 @@
+
+import QueryProvider from "@/providers/QueryProvider";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <QueryProvider>
+          {children}
+        </QueryProvider>
+      </body>
+    </html>
+  );
+}
